ab863e559ee249d185b46a309e23ecfa7807fb23
Vishal V <168957641+Vis-halV@users.noreply.github.com>
2025-09-09 03:58:19 +0000

    chore: Tailwind CSS setup

--- ecommerce-auth/src/App.jsx
+++ ecommerce-auth/src/App.jsx
@@ -1,20 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
+// import ForgotPassword from "./pages/ForgotPassword";
+// import ResetPassword from "./pages/ResetPassword";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/forgot-password" element={<ForgotPassword />} />
+//         <Route path="/reset-password" element={<ResetPassword />} />
+//         <Route path="*" element={<Login />} /> {/* fallback route */}
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="*" element={<Login />} /> {/* fallback route */}
-      </Routes>
-    </Router>
+    <h1 className="text-3xl font-bold underline bg-blue-200">
+    Hello world!
+    </h1>
   );
 }
 
